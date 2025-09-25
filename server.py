3bcfaac72ce8e5e10b041096dc3023fb14ad8f35
shuo jiang <shuovok@gmail.com>
2025-09-25 08:55:05 -0500

    added guest book

--- server.py
+++ server.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+import os
+import psycopg2
+
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+data = []
+
+@app.route('/',methods=['GET','POST'])
+def hello():
+    if request.method == 'POST':
+        data.append({'username': request.form.get('username'), 'comment':request.form.get('comment')})
+        print(data)
+    return render_template('index.html', data=data)
